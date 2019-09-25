52538c30ef9df5dbd200cfd5c750a0336a7b3169
Richard Yang <ruiqi888@gmail.com>
2019-09-25 09:16:19 -0700

    Move 8ball cog into fun cog

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -1,4 +1,5 @@
 import os
+import random
 import discord
 from discord.ext import commands
 
@@ -6,12 +7,23 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.src_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+
+    @commands.command(name='8ball')
+    async def eightball(self, ctx):
+        channel = ctx.message.channel
+        if ctx.message.content.strip() == "-8ball":
+            img = discord.File(os.path.join(self.src_dir,'img/8ball_0.png'))
+            await channel.send("Type a question after the command!", file=img)
+        else:
+            r = random.randint(1,5)
+            img = discord.File(os.path.join(self.src_dir,'img/8ball_{}.png'.format(r)))
+            await channel.send(file=img)
     
     @commands.command()
     async def billy(self, ctx):
         print("Press F for Billy")
         channel = ctx.message.channel
-        file = discord.File(os.path.join(self.src_dir, 'cache/billy.gif'))
+        file = discord.File(os.path.join(self.src_dir, 'img/billy.gif'))
         msg = await channel.send("Press F to pay respects", file=file)
         await msg.add_reaction(emoji="🇫")
 
