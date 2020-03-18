0eb9d78a7b0ba91925e8a05c36fda0ed278850e8
Richard Yang <ruiqi888@gmail.com>
2020-03-17 20:16:20 -0700

    FFXIV module

---
+++ src/cogs/ffxiv.py
@@ -0,0 +1,46 @@
+import pymysql
+import discord
+import requests
+
+from config import conf
+from contextlib import closing
+from discord.ext import tasks, commands
+
+class FFXIV(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = self.bot.get_cog('Database').db
+        self.economy_cog = self.bot.get_cog('Economy')
+
+        with closing(self.db.cursor()) as cursor:
+            # Create `users` table if it does not exist
+            cursor.execute('''CREATE TABLE IF NOT EXISTS ffxiv (id BIGINT PRIMARY KEY, ffxivid BIGINT)''')
+    
+    @commands.command()
+    async def linkff14(self, ctx, first_name:str, last_name:str, server:str):
+        params = {'name': '{} {}'.format(first_name, last_name), 'server': server, 'private_key': conf.get("FF14_TOKEN")}
+        channel = ctx.message.channel
+        author_id = ctx.message.author.id
+        try:
+            r = requests.get(url="http://xivapi.com/character/search", params=payload).json()
+            player = r['Results'][0]
+            if player['Name'] != "{} {}".format(first_name, last_name):
+                await channel.send("Player not found, did you mean {}? Please try the command again.".format(player['Name']))
+                return
+            else:
+                embed = discord.Embed(title="Your character has been linked to your Discord!")
+                embed.set_thumbnail(url=player['Avatar'])
+                embed.add_field(name="Name:", value="{} {}".format(first_name, last_name), inline=False)
+                embed.add_field(name="Server:", value=player['Server'].replace('\\xa0', ' '), inline=False)
+                await channel.send(embed=embed)
+                with closing(self.db.cursor()) as cursor:
+                    # Create `users` table if it does not exist
+                    cursor.execute('INSERT INTO ffxiv (id, ffxivid) VALUES(%s, %s) ON DUPLICATE KEY UPDATE id=%s, ffxivid=%s', (author_id, player['ID'], author_id, player['ID']))
+                return
+                
+        except:
+            await channel.send("Something went wrong while reaching the FFXIV servers. Please try again later.")
+            return
+
+def setup(bot):
+    bot.add_cog(FFXIV(bot))
